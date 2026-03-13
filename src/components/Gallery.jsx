51adc0d0c0390deab5cf3c2b771b6cbb322028a5
Swayam <swayamkumar226@gmail.com>
2026-03-13 18:36:34 +0530

    spinner added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,17 @@
 import PhotoCard from "./PhotoCard";
+import Spinner from "./Spinner";
 import useFetchPhotos from "../hooks/useFetchPhotos";
 
 function Gallery() {
   const { photos, loading, error } = useFetchPhotos();
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <p className="text-center text-red-500 mt-10">{error}</p>;
+  }
 
   return (
     <div className="max-w-7xl mx-auto p-4">
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className="flex justify-center items-center py-20">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default Spinner;
